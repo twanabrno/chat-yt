46eec0cfdcb4876fefcc1b5672716c641f4e7d3a
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-06-17 19:17:03 +0300

    messages and conversations

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,12 +1,23 @@
+import useGetConversations from "../../hooks/useConversation";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 export default function Conversations() {
+  const { loading, conversations } = useGetConversations();
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
- 
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length - 1}
+        />
+      ))}
+
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
     </div>
   );
 }
