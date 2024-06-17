68c613fe1f80b253f2cf34ecced5e2924babd4f4
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-06-17 21:33:03 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -12,6 +13,8 @@ import { app,server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRouters);
 app.use("/api/message", messageRouters);
 app.use("/api/users", userRouters);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
