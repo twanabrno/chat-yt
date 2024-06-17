80b0b76beee68f607ff18c06bb9dd59332325268
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-06-17 18:48:20 +0300

    aignup/signin authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Home from "./pages/home/Home";
-
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-3 h-screen flex items-center justify-center">
-      <Home />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <Signup />}
+        />
+      </Routes>
+      <Toaster />
     </div>
   );
 }
