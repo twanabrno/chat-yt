8d49e4cea7eb45e6f80372500ec444326e9cb90b
twanakhudhur <twana.ahmed.5595@gmail.com>
2024-06-17 17:28:45 +0300

    UI Design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,12 @@
+import Conversation from "./Conversation";
+
+export default function Conversations() {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+ 
+    </div>
+  );
+}
